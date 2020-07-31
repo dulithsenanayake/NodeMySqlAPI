1ae7b27a8c44c503717f75a8210ced607c2ea76e
dulithsenanyake <dulithcsenanayake@gmail.com>
2020-08-01 02:26:46 +0530

    fixed mysql error

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const db = require('./configuration/config');
+const db = require('./configuration/config');
 const dotenv = require('dotenv')
 const cors = require('cors');
 
 // listen to DB
-// db.authenticate()
-//     .then(() => console.log('MySQL Database Connected'))
-//     .catch(err => console.log('Error: ' + err) );
+db.authenticate()
+    .then(() => console.log('MySQL Database Connected'))
+    .catch(err => console.log('Error: ' + err) );
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
-// app.use('/student',require('./routes/studentR'));
+app.use('/student',require('./routes/studentR'));
 
 app.get("/", (req, res) =>
   res.send("Hello Wolrd!")
