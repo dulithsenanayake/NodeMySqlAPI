03e3fd5988ced4195d20e8d93c69bb3a6d04d875
dulithsenanyake <dulithcsenanayake@gmail.com>
2020-08-01 02:30:13 +0530

    fixed ssl error

--- configuration/config.js
+++ configuration/config.js
@@ -3,10 +3,6 @@ const fs = require('fs');
 const env = require('dotenv').config();
 const sequelize = new Sequelize('uokse', process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    ssl: {
-     key: fs.readFileSync('./certs/client-key.pem'),
-     cert: fs.readFileSync('./certs/client-cert.pem')
-    },
     dialect: "mysql",
     logging: false,
     pool: {
